55c57d33b76fa673ecb84ff545a7320495fc7955
looiic <loic.jermann@gmail.com>
2024-01-15 13:24:49 +0100

    Added an icon and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,36 +2,6 @@
   import "./styles.css";
 </script>
 
-<!--
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Zitat App</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/zitate">Zitate</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/gruppen">Gruppen</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
--->
-
 <div class="container">
   <slot />
 </div>
